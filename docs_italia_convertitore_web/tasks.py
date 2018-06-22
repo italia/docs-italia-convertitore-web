f607522818b81d47607d35bc509345344a5ce869
Paolo Romolini <p.romolini@nephila.it>
2018-06-22 17:18:06 +0200

    fix the bug that prevented the celery task to work

--- docs_italia_convertitore_web/tasks.py
+++ docs_italia_convertitore_web/tasks.py
@@ -1,13 +1,10 @@
 """Basic tasks."""
 from __future__ import absolute_import
-import logging
 
-from base64 import urlsafe_b64encode
+import logging
 import os
 import subprocess
-import shutil
 
-from django.core.files.storage import default_storage
 from django.conf import settings
 from django.core.mail import send_mail
 
@@ -15,37 +12,34 @@ from readthedocs.worker import app
 
 log = logging.getLogger(__name__)
 
-CONVERSION_UPLOAD_DIR = os.path.join(settings.MEDIA_ROOT, 'tmp')
+PRODUCTION_DOMAIN = getattr(settings, 'PRODUCTION_DOMAIN')
+DOCS_ITALIA_CONVERSION_UPLOAD_PATH = os.path.join(settings.MEDIA_URL, 'tmp')
+DOCS_ITALIA_CONVERTER_EMAIL = getattr(settings, 'DOCS_ITALIA_CONVERTER_EMAIL', 'info@example.org')
 
 
 @app.task(queue='web')
-def process_file(email, uploaded):
+def process_file(email, saved, unique_key):
     """
     This celery task save the uploaded file from memory to the
     tmp folder and calls the `converti` command on it.
     Than emails the user with the link to download the converted file.
+    :param email: the user email
+    :param saved: The full path of the uploaded file
+    :parma unique_key: the unique name of the new folder
     """
-    unique_key = urlsafe_b64encode(email + uploaded.name).rstrip('=')# maybe we should use date time now to allow the upload of a file with the same name
-    new_folder_name = os.path.join(CONVERSION_UPLOAD_DIR, unique_key)
-    if not os.path.exists(new_folder_name):
-        os.mkdir(new_folder_name)
-    file_path = os.path.join(new_folder_name, uploaded.name)
-    saved = os.path.join(CONVERSION_UPLOAD_DIR, new_folder_name, uploaded.name)
-    with default_storage.open(saved, 'wb+') as destination:
-        for chunk in uploaded.chunks():
-            destination.write(chunk)
-    subprocess.call(['converti',  saved], cwd=new_folder_name)
-    zipped_name = os.path.splitext(uploaded.name)[0]
-    shutil.make_archive( #  create the archive
-        os.path.join(new_folder_name, zipped_name),
-        'zip',
-        os.path.join(new_folder_name,'risultato-conversione')
-    )
-    log.info('Processing uploaded file {} from {}'.format(uploaded.name, email))
+    file_path, file_name = os.path.split(saved)
+    new_file_name = os.path.splitext(os.path.basename(file_name))[0] + '.rst'
+    with open(os.path.join(file_path, new_file_name), 'w') as out:
+        subprocess.call(['pandoc', saved], stdout=out)
+    log.info('Processing uploaded file {} from {}'.format(saved, email))
     send_mail(
         'Conversione del documento effettuata',
-        'http://localhost:8000/media/tmp/{}/{}.zip'.format(unique_key, zipped_name),
-        'from@example.com',
+        'http://{}{}/{}/{}'.format(
+            PRODUCTION_DOMAIN, DOCS_ITALIA_CONVERSION_UPLOAD_PATH,
+            unique_key,
+            new_file_name
+        ),
+        DOCS_ITALIA_CONVERTER_EMAIL,
         [email],
         fail_silently=False,
     )
