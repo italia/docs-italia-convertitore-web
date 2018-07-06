14517d903da58deff619c25c504551659f5f4ce8
Iacopo Spalletti <i.spalletti@nephila.it>
2018-07-06 18:56:12 +0200

    Fix errors in handling conversions
    * Handle spaces and special characters in original file names
    * Fix pandoc arguments

--- docs_italia_convertitore_web/tasks.py
+++ docs_italia_convertitore_web/tasks.py
@@ -4,12 +4,14 @@ from __future__ import absolute_import, unicode_literals
 
 import logging
 import os
-import shutil
 import subprocess
+from shutil import make_archive
 
 from celery import shared_task
 from django.conf import settings
 from django.core.mail import send_mail
+from django.utils.encoding import force_text
+from django.utils.http import urlquote_plus
 
 log = logging.getLogger(__name__)
 
@@ -18,48 +20,93 @@ DOCS_ITALIA_CONVERSION_UPLOAD_PATH = os.path.join(settings.MEDIA_URL, 'tmp')
 DOCS_ITALIA_CONVERTER_EMAIL = getattr(settings, 'DOCS_ITALIA_CONVERTER_EMAIL', 'info@example.org')
 
 
-@shared_task(queue='web')
-def process_file(email, saved, unique_key):
+def _run_converti(uploaded_file, new_file_name, file_path):
     """
-    This celery task save the uploaded file from memory to the
-    tmp folder and calls the `pandoc` command on it.
-    Than emails the user with the link to download the converted file.
+    Run converti and report output messages
 
-    :param email: the user email
-    :param saved: the full path of the uploaded file
-    :param unique_key: the unique name of the new folder
+    :param uploaded_file: uploaded file
+    :param new_file_name: converted file name
+    :param file_path: source path
+
+    :return: error messages
     """
-    file_path, file_name = os.path.split(saved)
-    new_file_name = '%s.rst' % os.path.splitext(os.path.basename(file_name))[0]
-    log.info('Processing uploaded file {} from {}'.format(saved, email))
     err_msg = None
     out_msg = None
+    try:
+        out_msg = subprocess.check_output(['converti', uploaded_file], stderr=subprocess.STDOUT, cwd=file_path)
+    except subprocess.CalledProcessError as e:
+        err_msg = e.output
+    except Exception as e:
+        err_msg = '%s - %s' % (out_msg, force_text(e))
+    return out_msg, err_msg
+
+
+def _run_pandoc(uploaded_file, new_file_name, file_path):
+    """
+    Run pandoc and report output messages
+
+    :param uploaded_file: uploaded file
+    :param new_file_name: converted file name
+    :param file_path: source path
+
+    :return: error messages
+    """
+    err_msg = None
+    out_msg = None
+    args = ['pandoc', uploaded_file, '-o', os.path.join(file_path, new_file_name)]
+    try:
+        out_msg = subprocess.check_output(args, stderr=subprocess.STDOUT)
+    except subprocess.CalledProcessError as e:
+        err_msg = '%s - %s' % (out_msg, force_text(e.output))
+    except Exception as e:
+        err_msg = '%s - %s' % (out_msg, force_text(e))
+    return out_msg, err_msg
+
+
+@shared_task(queue='web')
+def process_file(email, uploaded_file, unique_key):
+    """
+    This celery task save the uploaded file from memory to the
+    tmp folder and calls the ``pandoc`` or ``converti`` command on it.
+    Than emails the user with the link to download the converted file or the error message
+
+    TODO: Report back to the frontend the conversion status
+
+    :param email: the user email
+    :param uploaded_file: the full path of the uploaded file
+    :param unique_key: the unique name of the new folder
+    """
+    file_path, file_name = os.path.split(uploaded_file)
+    new_file_name = '%s.rst' % os.path.splitext(os.path.basename(file_name))[0]
+    log.info('Processing uploaded file {} from {}'.format(uploaded_file, email))
     if os.path.splitext(file_name)[1].lower() == '.docx':
-        try:
-            out_msg = subprocess.check_output(['converti', saved], stderr=subprocess.STDOUT, cwd=file_path)
-        except subprocess.CalledProcessError as e:
-            err_msg = e.output
+        out_msg, err_msg = _run_converti(uploaded_file, new_file_name, file_path)
         if not err_msg:
             new_file_name = os.path.splitext(os.path.basename(file_name))[0]
             os.chdir(os.path.join(file_path, 'risultato-conversione'))
-            shutil.make_archive(os.path.join(file_path, new_file_name), 'zip', '.')
+            make_archive(os.path.join(file_path, new_file_name), 'zip', '.')
             new_file_name = '%s.zip' % new_file_name
     else:
-        try:
-            out_msg = subprocess.check_output(['pandoc', saved, '-o %s' % new_file_name], stderr=subprocess.STDOUT)
-        except subprocess.CalledProcessError as e:
-            err_msg = e.output
-    msg = '{}/{}/{}'.format(
-        DOCS_ITALIA_CONVERSION_UPLOAD_PATH,
-        unique_key,
-        new_file_name
-    )
+        out_msg, err_msg = _run_pandoc(uploaded_file, new_file_name, file_path)
     if err_msg:
-        log.error('There was an error processing %s: %s %s' % (saved, out_msg, err_msg))
-    send_mail(
-        'Conversione documento di DOCS ITALIA',
-        msg,
-        DOCS_ITALIA_CONVERTER_EMAIL,
-        [email],
-        fail_silently=False,
-    )
+        log.error('There was an error processing %s: %s %s' % (uploaded_file, out_msg, err_msg))
+        send_mail(
+            'Errore conversione documento di DOCS ITALIA',
+            err_msg,
+            DOCS_ITALIA_CONVERTER_EMAIL,
+            [email],
+            fail_silently=False,
+        )
+    else:
+        msg = '{}/{}/{}'.format(
+            DOCS_ITALIA_CONVERSION_UPLOAD_PATH,
+            unique_key,
+            urlquote_plus(new_file_name)
+        )
+        send_mail(
+            'Conversione documento di DOCS ITALIA',
+            msg,
+            DOCS_ITALIA_CONVERTER_EMAIL,
+            [email],
+            fail_silently=False,
+        )
--- tests/test_tasks.py
+++ tests/test_tasks.py
@@ -1,2 +1,64 @@
 # -*- coding: utf-8 -*-
 from __future__ import absolute_import, print_function, unicode_literals
+
+import os
+from tempfile import mkstemp
+from unittest.mock import patch
+
+from django.core import mail
+from django.test import TestCase
+from docs_italia_convertitore_web.tasks import process_file
+
+
+class TaskTest(TestCase):
+
+    @patch('docs_italia_convertitore_web.tasks._run_pandoc')
+    def test_pandoc_error(self, patched):
+        error_msg = 'errored'
+        patched.return_value = (
+            None, error_msg
+        )
+        process_file.apply(('test@email.com', 'some file.doc', 'super_unique'), )
+        self.assertEqual(len(mail.outbox), 1)
+        message = mail.outbox[0]
+        self.assertEqual(message.subject, 'Errore conversione documento di DOCS ITALIA')
+        self.assertEqual(message.body, error_msg)
+
+    @patch('docs_italia_convertitore_web.tasks._run_pandoc')
+    def test_pandoc_run(self, patched):
+        patched.return_value = (
+            None, None
+        )
+        process_file.apply(('test@email.com', 'some file.doc', 'super_unique'), )
+        self.assertEqual(len(mail.outbox), 1)
+        message = mail.outbox[0]
+        self.assertEqual(message.subject, 'Conversione documento di DOCS ITALIA')
+        self.assertEqual(message.body, 'http://convert.com/media/tmp/super_unique/some+file.rst')
+
+    @patch('docs_italia_convertitore_web.tasks._run_converti')
+    def test_converti_error(self, patched):
+        error_msg = 'errored'
+        patched.return_value = (
+            None, error_msg
+        )
+        process_file.apply(('test@email.com', 'some file.docx', 'super_unique'), )
+        self.assertEqual(len(mail.outbox), 1)
+        message = mail.outbox[0]
+        self.assertEqual(message.subject, 'Errore conversione documento di DOCS ITALIA')
+        self.assertEqual(message.body, error_msg)
+
+    @patch('docs_italia_convertitore_web.tasks._run_converti')
+    @patch('docs_italia_convertitore_web.tasks.make_archive')
+    def test_converti_run(self, make_archive, _run_converti):
+        fp, path = mkstemp(suffix='.docx')
+        _run_converti.return_value = (
+            None, None
+        )
+        make_archive.return_value = True
+        os.makedirs(os.path.join(os.path.dirname(path), 'risultato-conversione'), exist_ok=True)
+        process_file.apply(('test@email.com', path, 'super_unique'), )
+        self.assertEqual(len(mail.outbox), 1)
+        message = mail.outbox[0]
+        new_file = os.path.splitext(os.path.basename(path))[0]
+        self.assertEqual(message.subject, 'Conversione documento di DOCS ITALIA')
+        self.assertEqual(message.body, 'http://convert.com/media/tmp/super_unique/%s.zip' % new_file)
