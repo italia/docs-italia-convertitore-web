bd6cc8628ee34240211472dca02b1922f4143278
Iacopo Spalletti <i.spalletti@nephila.it>
2018-07-25 08:08:11 +0200

    Add support for converter options

--- docs_italia_convertitore_web/forms.py
+++ docs_italia_convertitore_web/forms.py
@@ -2,11 +2,52 @@
 """Public project views."""
 from __future__ import absolute_import, unicode_literals
 
+import json
+import os
+
 from django import forms
+from django.utils.translation import ugettext_lazy as _
 
 
 class ItaliaConverterForm(forms.Form):
     """ Italia converter Form """
-    email = forms.EmailField()
-    file = forms.FileField()
-    monospace = forms.BooleanField(required=False)
+    email = forms.EmailField(label=_('email'), required=True)
+    file = forms.FileField(label=_('file'), required=False)
+    normattiva = forms.BooleanField(
+        label=_('Collegamenti normattiva'), required=False,
+        help_text=_('<a target="blank" href="https://github.com/italia/docs-italia-comandi-conversione/blob/'
+                    'master/doc/comandi/converti-opzioni.md#collegamento-normattiva">Informazioni di dettaglio</a>')
+    )
+    celle_complesse = forms.BooleanField(
+        label=_('Celle complesse'), required=False,
+        help_text=_('<a target="blank" href="https://github.com/italia/docs-italia-comandi-conversione/blob/'
+                    'master/doc/comandi/converti-opzioni.md#celle-complesse">Informazioni di dettaglio</a>')
+    )
+    preserva_citazioni = forms.BooleanField(
+        label=_('Preserva citazioni'), required=False,
+        help_text=_('<a target="blank" href="https://github.com/italia/docs-italia-comandi-conversione/blob/'
+                    'master/doc/comandi/converti-opzioni.md#preserva-citazioni">Informazioni di dettaglio</a>')
+    )
+    dividi_sezioni = forms.BooleanField(
+        label=_('Dividi sezioni'), required=False,
+        help_text=_('<a target="blank" href="https://github.com/italia/docs-italia-comandi-conversione/blob/'
+                    'master/doc/comandi/converti-opzioni.md#dividi-sezioni">Informazioni di dettaglio</a>')
+    )
+
+    converter_options = ['normattiva', 'celle_complesse', 'preserva_citazioni', 'dividi_sezioni']
+
+    def get_options_json(self, folder):
+        """ Saves the converter options as json file suitable for `converti` command """
+        options = {
+            # Rif italia/docs-italia-comandi-conversione/doc/comandi/converti-opzioni.md#dividi-sezioni
+            'dividi-sezioni': self.cleaned_data['dividi_sezioni'],
+            # Rif italia/docs-italia-comandi-conversione/doc/comandi/converti-opzioni.md#collegamento-normattiva
+            'collegamento-normattiva': self.cleaned_data['normattiva'],
+            # Rif italia/docs-italia-comandi-conversione/doc/comandi/converti-opzioni.md#celle-complesse
+            'celle-complesse': self.cleaned_data['celle_complesse'],
+            # Rif italia/docs-italia-comandi-conversione/doc/comandi/converti-opzioni.md#preserva-citazioni
+            'preserva-citazioni': self.cleaned_data['preserva_citazioni'],
+        }
+        with open(os.path.join(folder, 'options.json'), 'w') as fp:
+            json.dump(options, fp)
+        return os.path.join(folder, 'options.json')
--- tests/test_tasks.py
+++ tests/test_tasks.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 from __future__ import absolute_import, print_function, unicode_literals
 
+import json
 import os
 from tempfile import mkstemp
 from unittest.mock import patch
@@ -92,3 +93,30 @@ class TaskTest(TestCase):
         self.assertEqual(message.subject, 'Conversione documento di DOCS ITALIA')
         self.assertIn('http://convert.com/media/tmp/super_unique/%s.zip' % new_file, message.body)
         self.assertIn('Conversion ID', message.body)
+
+    @patch('docs_italia_convertitore_web.tasks.subprocess.check_output')
+    @patch('docs_italia_convertitore_web.tasks.make_archive')
+    @patch('docs_italia_convertitore_web.tasks.sentry_message')
+    def test_converti_json(self, sentry_message, make_archive, check_output):
+        fp, path = mkstemp(suffix='.docx')
+        check_output.return_value = True
+        make_archive.return_value = True
+        sentry_message.return_value = True
+
+        base_dir = os.path.dirname(path)
+        os.makedirs(os.path.join(base_dir, 'risultato-conversione'), exist_ok=True)
+        json_options = os.path.join(base_dir, 'options.json')
+        options = {
+            'normattiva': True,
+            'celle-complesse': False,
+            'preserva-citazioni': True,
+            'dividi-sezioni': False
+        }
+        with open(json_options, 'w') as fp:
+            json.dump(options, fp)
+
+        process_file.apply(('test@email.com', path, 'super_unique', True, json_options), )
+        sentry_message.assert_not_called()
+        self.assertEqual(check_output.call_count, 1)
+        args = ['converti', path, '--opzioni-json', json_options]
+        self.assertEqual(args, list(check_output.call_args)[0][0])
