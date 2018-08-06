463d63921679e99be97d89416bb2924e075a0a65
Francesco Zaia <francesco.zaia@gmail.com>
2018-08-06 15:36:50 +0200

    Fixed a few things on the Converter UI, moved JS to a separate file.

--- docs_italia_convertitore_web/forms.py
+++ docs_italia_convertitore_web/forms.py
@@ -11,27 +11,27 @@ from django.utils.translation import ugettext_lazy as _
 
 class ItaliaConverterForm(forms.Form):
     """ Italia converter Form """
-    email = forms.EmailField(label=_('email'), required=True)
+    email = forms.EmailField(label=_('email'), required=True, widget=forms.EmailInput(attrs={'class': 'form-control', 'data-msg': 'Email non valida'}))
     file = forms.FileField(label=_('file'), required=False)
     normattiva = forms.BooleanField(
         label=_('Collegamenti normattiva'), required=False,
-        help_text=_('<a target="blank" href="https://github.com/italia/docs-italia-comandi-conversione/blob/'
-                    'master/doc/comandi/converti-opzioni.md#collegamento-normattiva">Informazioni di dettaglio</a>')
+        help_text=_('<p>Con questa opzione abilitata, vengono trasformati tutti i riferimenti a leggi e normative in collegamenti ipertestuali al sito di Normattiva.</p><p><a target="blank" href="https://github.com/italia/docs-italia-comandi-conversione/blob/'
+                    'master/doc/comandi/converti-opzioni.md#collegamento-normattiva" class="d-block text-uppercase font-weight-bold x-small">Vai alla linea guida →</a></p>')
     )
     celle_complesse = forms.BooleanField(
         label=_('Celle complesse'), required=False,
-        help_text=_('<a target="blank" href="https://github.com/italia/docs-italia-comandi-conversione/blob/'
-                    'master/doc/comandi/converti-opzioni.md#celle-complesse">Informazioni di dettaglio</a>')
+        help_text=_('<p>Con questa opzione abilitata, il processo di conversione evita di mandare a capo il contenuto delle celle mantenendolo tutto sulla stessa riga.</p><p><a target="blank" href="https://github.com/italia/docs-italia-comandi-conversione/blob/'
+                    'master/doc/comandi/converti-opzioni.md#celle-complesse" class="d-block text-uppercase font-weight-bold x-small">Vai alla linea guida →</a></p>')
     )
     preserva_citazioni = forms.BooleanField(
         label=_('Preserva citazioni'), required=False,
-        help_text=_('<a target="blank" href="https://github.com/italia/docs-italia-comandi-conversione/blob/'
-                    'master/doc/comandi/converti-opzioni.md#preserva-citazioni">Informazioni di dettaglio</a>')
+        help_text=_('<p>Con questa opzione abilitata, il testo indentato viene interpretato come citazione.</p><p><a target="blank" href="https://github.com/italia/docs-italia-comandi-conversione/blob/'
+                    'master/doc/comandi/converti-opzioni.md#preserva-citazioni" class="d-block text-uppercase font-weight-bold x-small">Vai alla linea guida →</a></p>')
     )
     dividi_sezioni = forms.BooleanField(
         label=_('Dividi sezioni'), required=False,
-        help_text=_('<a target="blank" href="https://github.com/italia/docs-italia-comandi-conversione/blob/'
-                    'master/doc/comandi/converti-opzioni.md#dividi-sezioni">Informazioni di dettaglio</a>')
+        help_text=_('<p>Con questa opzione abilitata, la struttura dei files viene divisa solo in base alle sezioni di primo livello.</p><p><a target="blank" href="https://github.com/italia/docs-italia-comandi-conversione/blob/'
+                    'master/doc/comandi/converti-opzioni.md#dividi-sezioni" class="d-block text-uppercase font-weight-bold x-small">Vai alla linea guida →</a></p>')
     )
 
     converter_options = ['normattiva', 'celle_complesse', 'preserva_citazioni', 'dividi_sezioni']
---
+++ docs_italia_convertitore_web/static/js/docs_italia_convertitore.js
@@ -0,0 +1,56 @@
+Dropzone.autoDiscover = false;
+$(document).ready(function() {
+    $(function () {
+        $('[data-toggle="popover"]').popover()
+    });
+    var validator = $('#converterForm').validate({
+        rules: {
+            email: {
+                required: true,
+                email: true
+            },
+            file: {
+                required: true
+            }
+        },
+        errorPlacement: function(error, element) {
+            error.appendTo('.errors');
+        }
+    });
+    new Dropzone('#fileInput', {
+        url: "#",
+        maxFiles:1,
+        acceptedFiles: "application/msword,application/vnd.openxmlformats-officedocument.wordprocessingml.document,application/vnd.oasis.opendocument.text,.md",
+        autoProcessQueue: false,
+        init: function () {
+            var myDropzone = this;
+            $("#submit").click(function (e) {
+                e.preventDefault();
+                myDropzone.processQueue();
+                if(!validator.form()) {
+                    alert("E' necessario fornire un indirizzo email");
+                }
+            });
+            myDropzone.on('success', function () {
+                window.location.href = "{% url 'docs_italia_convertitore:conversion-started' %}";
+            });
+        },
+        sending: function (file, xhr, formData) {
+            formData.append("email", $('#id_email').val());
+            formData.append("monospace", $('#id_monospace').val());
+            formData.append("csrfmiddlewaretoken", $('input[name="csrfmiddlewaretoken"]').val());
+        },
+        dictDefaultMessage: "Trascina qui i file o clicca per caricarli",
+        dictFallbackMessage: "Il tuo browser non supporta alcune funzioni di caricamento file",
+        dictFallbackText: "Il file che stai provando a caricare non è supportato",
+        dictFileTooBig: "Il file che stai tentando di caricare è troppo pesante",
+        dictInvalidFileType: "Il file che stai provando a caricare non è supportato",
+        dictResponseError: "Il server non permette il caricamento del file, prova a riaggiornare la pagina",
+        dictCancelUpload: "Annulla",
+        dictUploadCanceled: "Caricamento annullato",
+        dictCancelUploadConfirmation: "Vuoi davvero interrompere il caricamento?",
+        dictRemoveFile: "Rimuovi",
+        dictMaxFilesExceeded: "Non puoi caricare altri file",
+        dictFileSizeUnits: "Non puoi superare il numero di file consentito per quest'area"
+    })
+})
